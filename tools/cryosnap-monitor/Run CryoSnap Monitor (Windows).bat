fae846a56155a6494835692d3bf0243986df46cd
conceptwiley <jon@conceptwiley.com>
2026-07-08 17:11:29 -0500

    Delete tools/cryosnap-monitor/Run CryoSnap Monitor (Windows).bat
    Signed-off-by: conceptwiley <jon@conceptwiley.com>

--- tools/cryosnap-monitor/Run CryoSnap Monitor (Windows).bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Double-click this file to start the CryoSnap Monitor.
-REM It runs the PowerShell launcher with script execution allowed for this run only.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0monitor_cryosnap.ps1"
